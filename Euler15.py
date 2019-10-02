106e856c930df2892e4dd737a278c5be7bf9b238
gprashant123 <37257549+gprashant123@users.noreply.github.com>
2019-10-02 23:13:50 +0530

    Create Euler15.py

---
+++ Euler15.py
@@ -0,0 +1,29 @@
+stored={}
+def rec(m,n):
+    try:
+        return stored[(m,n)]
+    except:
+        if m==1 and n==1:
+            stored[(m,n)]=2
+            return 2
+        elif m==1 or n==1:
+            if m==1:
+                a=rec(1,n-1)+1
+                stored[(m,n)]=a
+                stored[(n,m)]=a
+                return a
+            else:
+                a=rec(1,m-1)+1
+                stored[(m,n)]=a
+                stored[(n,m)]=a
+                return rec(1,m-1)+1
+        else:
+            if m==n:
+                b=2*rec(m,n-1)
+                stored[(m,n)]=b
+                return b
+            else:
+                b=rec(m-1,n)+rec(m,n-1)
+                stored[(m,n)]=b
+                stored[(n,m)]=b
+                return b
